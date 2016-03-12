490f64f7ed728beac40a6717b6660e575bd002cb
splincode <omaxphp@yandex.ru>
2016-03-12 18:47:27 +0300

    update

--- install/apachengine.sh
+++ install/apachengine.sh
@@ -17,7 +17,7 @@ echo "Успешно добавлен хост -- apachengine"
 
 # настраиваем хост
 sudo chmod 777 /etc/hosts
-sudo echo "127.0.0.1   apachengine" >> /etc/hosts
+sudo echo -e "\\n127.0.0.1   apachengine\\n" >> /etc/hosts
 sudo chmod 755 /etc/hosts
 read -p "Добавлен хост [127.0.0.1   apachengine]. Дальше? [Д/н] " answer
 
@@ -31,6 +31,15 @@ sudo a2ensite apachengine.conf
 
 sudo chmod -R 777 * /var/www/
 sudo chown -R $USER:$USER /var/www/apachengine/
+
+# баг c правами www-data
+read -p "Имя пользователя: " user
+sudo useradd -G www-data $user
+
+sudo echo -e "\\n umask 002 \\n" >> /etc/init.d/apache2
+sudo echo -e "\\n umask 002 \\n" >> /etc/apache2/envvars
+
+sudo systemctl daemon-reload
 
 # настраиваем хост
 sudo service apache2 start
